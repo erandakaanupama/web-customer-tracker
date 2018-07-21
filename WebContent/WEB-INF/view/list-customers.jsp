2c32057e19af6b660ca4d27284e0a0175ab38ffd
erandakaanupama <erandakaanupama@gmail.com>
2018-07-21 09:41:04 +0530

    added DAO and customer mapping

---
+++ WebContent/WEB-INF/view/list-customers.jsp
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>customer-list</title>
+</head>
+<body>
+List customers ...
+</body>
+</html>
---
+++ src/com/eab/springdemo/dao/CustomerDAOIml.java
@@ -0,0 +1,38 @@
+package com.eab.springdemo.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.eab.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOIml implements CustomerDAO {
+
+	// inject session factory
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+
+		// get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+
+		// create the query
+		Query<Customer> theQuery = currentSession.createQuery("from customer", Customer.class);
+
+		// exec query
+		List<Customer> customers = theQuery.getResultList();
+
+		// return results
+		return customers;
+	}
+
+}
